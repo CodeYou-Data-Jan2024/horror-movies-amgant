1b946f7d820b420f6ac3ab70129e982dacbe5c09
Aliyah Gant <aliyahgant5@gmail.com>
2024-06-29 18:35:37 -0400

    ready to go

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,22 @@
--- Add your SQL here
+/*
+Created by: Aliyah Gant
+CreateDate: 06/29/2024
+Description: This query contains the id, name and imdb rating of the top 3 movies in the horror genre from 1985 and earlier!
+*/
+
+SELECT
+	id AS 'Movie_ID',
+	name AS 'Movie_Title',
+	imdb_rating AS 'Rating'
+
+FROM
+	movies
+
+WHERE	
+	genre = 'horror' AND
+	year <= 1985
+
+ORDER BY
+	imdb_rating DESC
+
+LIMIT 3;-- Add your SQL here
